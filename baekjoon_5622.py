99367a750e2221773e10c6761e97ab946f9bef07
kdkkdkkdkkdkkdkkdk <60931880+kdkkdkkdkkdkkdkkdk@users.noreply.github.com>
2022-01-13 22:52:20 +0900

    Update and rename baekjoon_5622 to baekjoon_5622.py

--- baekjoon_5622.py
+++ baekjoon_5622.py
@@ -13,13 +13,13 @@ for y in alp:
 
 print(num)
 
-코드 한줄 줄이기↓
+#코드 한줄 줄이기↓
 
-for y in alp:
-  for i in dial:
-    if y in i
-      num += dial.index(i)+3
+#for y in alp:
+#  for i in dial:
+#    if y in i
+#      num += dial.index(i)+3
 
-백준 제출 결과, 동일함.
-메모리 : 30860 KB, 시간 : 68 ms
-코드 길이만 214 B VS. 187 B
+#백준 제출 결과, 동일함.
+#메모리 : 30860 KB, 시간 : 68 ms
+#코드 길이만 214 B VS. 187 B
