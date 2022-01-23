3e8d308ec7a9923380bc68102e2c4ff4e6e6ea84
name <email>
2022-01-24 00:32:09 +0900

    TIL Upload

---
+++ Python/Baekjoon/baekjoon_1978.py
@@ -0,0 +1,20 @@
+# baekjoon_1978.py
+# 소수찾기
+
+#루트 **(1/2)
+
+iter = int(input())
+nums = list(map(int, input().split()))
+prime = 0
+
+for i in nums:
+    cnt = 0  # 매번 0으로 초기화되야함
+    if i == 1:  # i == 1 일 때 넘기기
+        pass
+    for j in range(2, i+1):  # 2부터 i까지 나누기
+        if i % j == 0:  # 나머지가 0이면 cnt + 1
+            cnt += 1
+    if cnt == 1:  # 소수라면 나머지가 0인 경우가 본인이 본인을 나눴을 때 뿐
+        prime += 1
+
+print(prime)
