0f25a3d3474b75805eb8f46a9e171c24ce894752
kdkkdkkdkkdkkdkkdk <kdkboy.dk@gmail.com>
2022-01-24 00:52:30 +0900

    Commit history 미반영 해결 test

--- Python/Baekjoon/baekjoon_2941.py
+++ Python/Baekjoon/baekjoon_2941.py
@@ -2,11 +2,12 @@
 # 크로아티아 알파벳
 
 s = input()
-check = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+check = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
 for i in check:
-  if i in s:
-    s = s.replace(i,'*')
+    if i in s:
+        s = s.replace(i, '*')
 
 print(len(s))
 
+##
