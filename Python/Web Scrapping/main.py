94a23e6ad6c0c72928e346b4d96015bcf9aaa3e0
kdkkdkkdkkdkkdkkdk <kdkboy.dk@gmail.com>
2022-02-13 04:23:38 +0900

    update

--- Python/Web Scrapping/main.py
+++ Python/Web Scrapping/main.py
@@ -1,3 +1,4 @@
+from tkinter import PAGES
 import requests
 # python -m pip install requests
 from bs4 import BeautifulSoup
@@ -6,10 +7,10 @@ from bs4 import BeautifulSoup
 indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 pagination = indeed_soup.find("div", {"class": "pagination"})
-pages = pagination.find_all('a')
+links = pagination.find_all('a')
 
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
+pages = []
+for link in links[:-1]:
+    pages.append(int(link.string))
 
-print(spans[:-1])
+max_page = pages[-1]
