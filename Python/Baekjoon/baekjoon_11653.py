402452f702b71263ffa8169ea402d058f791ff76
kdkkdkkdkkdkkdkkdk <kdkboy.dk@gmail.com>
2022-02-13 23:58:26 +0900

    update

--- Python/Baekjoon/baekjoon_11653.py
+++ Python/Baekjoon/baekjoon_11653.py
@@ -1,4 +1,14 @@
 # baekjoon_11653.py
 # 소인수분해
 
+import sys
+input = sys.stdin.readline
+
 num = int(input())
+i = 2
+while num >= i:
+    if num % i == 0:
+        num = num // i
+        print(i)
+    else:
+        i += 1
