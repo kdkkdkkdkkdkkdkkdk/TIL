69d7714b7bacf5f54f50fa048fc714f460532c8f
kdkkdkkdkkdkkdkkdk <kdkboy.dk@gmail.com>
2022-02-14 00:04:43 +0900

    update

--- Python/Baekjoon/baekjoon_11653.py
+++ Python/Baekjoon/baekjoon_11653.py
@@ -1,14 +1,17 @@
 # baekjoon_11653.py
 # 소인수분해
 
-import sys
-input = sys.stdin.readline
-
 num = int(input())
 i = 2
-while num >= i:
+while num > 1:
     if num % i == 0:
         num = num // i
         print(i)
     else:
         i += 1
+
+# 시간 줄여보기
+# import sys + while num > 1: -- 30864KB / 1480ms
+# import sys + while num >= i: -- 30864KB / 1628ms
+# while num > 1: -- 30864KB / 1440ms
+# while num >= i: -- 30864KB / 1808ms
